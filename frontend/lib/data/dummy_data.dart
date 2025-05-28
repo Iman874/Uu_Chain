612b69e43f9e84d76011c852f860e4cea1fd4c0a
Rosul Iman <117097805+Iman874@users.noreply.github.com>
2025-05-28 11:14:56 +0700

    update

--- frontend/lib/data/dummy_data.dart
+++ frontend/lib/data/dummy_data.dart
@@ -3,10 +3,10 @@ import '../models/user.dart';
 const dummyUsers = [
   User(
     name: 'Admin',
-    privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80',
+    privateKey: '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80', // ganti privkey ini dengan yang sesuai
   ),
   User(
     name: 'User 1',
-    privateKey: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d',
+    privateKey: '0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d', // ganti privkey ini dengan yang sesuai
   ),
 ];
